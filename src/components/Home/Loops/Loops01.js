75ffa63f68122a87911865fd46f23b0f477f8cde
MMarek <mildnermarek@gmail.com>
2020-03-14 09:30:51 +0100

    mouse move on/off propo

--- src/components/Home/Loops/Loops01.js
+++ src/components/Home/Loops/Loops01.js
@@ -3,7 +3,35 @@ import '../../../scss/_loops.scss'
 
 class Square extends Component {
 
+    state = {
+        open: true,
+        one: false
+    }
+
+    handleButtonOneOn = () => {
+        this.setState({one: true});
+        console.log('najazd');
+    };
+
+    handleButtonOneOff = () => {
+        this.setState({one: false});
+        console.log('zjazd');
+    };
+
+    handleButtonOneClick = () => {
+        console.log('klik');
+    };
+
     render() {
+
+        const {open} = this.state;
+        let show = open ? null : 'hide';
+        const {one} = this.state;
+
+        let styleOne = {
+            fontSize: one ? '100px' : '20px',
+            transition: '5s',
+        };
 
         let n = 5;
         let result1 = 1;
@@ -18,16 +46,21 @@ class Square extends Component {
             j++;
         }
         return (
-            <section className='loops'>
-                <div className='squareFor'>
-                    Śilnia liczby {n} wynosi {result1} - wykonane pętlą for
-                </div>
+            <section className='loops' style={styleOne}
+                     onClick={this.handleButtonOneClick}
+                     onMouseEnter={this.handleButtonOneOn}
+                     onMouseLeave={this.handleButtonOneOff}>
+                {/*<nav className={show}>*/}
+                    <div className='squareFor'>
+                        Śilnia liczby {n} wynosi {result1} - wykonane pętlą for
+                    </div>
 
-                <h4> Zapraszam do prześledzenia kodu :) </h4>
+                    <h4> Zapraszam do prześledzenia kodu :) </h4>
 
-                <div className='squareWhile'>
-                    Śilnia liczby {n} wynosi {result2} - wykonane pętlą while
-                </div>
+                    <div className='squareWhile'>
+                        Śilnia liczby {n} wynosi {result2} - wykonane pętlą while
+                    </div>
+                {/*</nav>*/}
             </section>
         )
     }
