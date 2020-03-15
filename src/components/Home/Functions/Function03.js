7abd19a414987fb1459e5a8e7945329b3173dee4
MMarek <mildnermarek@gmail.com>
2020-03-15 16:05:27 +0100

    exercise function 03 + error

---
+++ src/components/Home/Functions/Function03.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+import '../../../scss/_exercises.scss'
+
+class Class extends Component {
+
+    state = {
+        click: false
+    };
+
+    handleButtonOneClick = () => {
+        this.setState({
+            click: this.state.click ? false : true
+        })
+    };
+
+    render() {
+
+        const {click} = this.state;
+
+        let styleOne = {
+            width: click ? '100%' : '10%',
+            height: click ? '100%' : '10%',
+            fontSize: click ? '15px' : '0%',
+            transition: '2s',
+        };
+
+        let styleTwo = {
+            fontSize: click ? '0%' : '70px',
+            transition: '5s',
+        };
+
+
+
+
+
+
+
+
+
+        return (
+            <section className='' style={styleOne}
+            onClick={this.handleButtonOneClick}>
+
+                <div className='numberOfExercise' style={styleTwo} onClick={this.handleButtonOneClick}>X.</div>
+
+                <div></div>
+                <div></div>
+
+            </section>
+        )
+    }
+}
+
+export default
